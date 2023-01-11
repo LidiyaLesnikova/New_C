5f3b52b78634016d700844c62da5659a00358f78
Lidiya <lesnikoval@yandex.ru>
2023-01-11 22:05:10 +0300

    Example 001 Hello World and 002 Hello User

---
+++ Example_002_HU/Program.cs
@@ -0,0 +1,6 @@
+﻿//Console.WriteLine("Введите ваше имя ");
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+//Console.WriteLine("Привет, ");
+Console.Write("Привет, ");
+Console.WriteLine(username);

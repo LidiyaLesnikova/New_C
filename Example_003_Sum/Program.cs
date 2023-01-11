8fbad6d2e45d591e3ae863597bcfb4f5101a32ff
Lidiya <lesnikoval@yandex.ru>
2023-01-11 23:00:26 +0300

    Example 003 Поменяли сумму двух чисел на два случайных числа

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,6 +1,6 @@
-﻿int numberA = 3;
+﻿int numberA = new Random().Next(1, 10);
 Console.Write(numberA);
-int numberB = 4;
+int numberB = new Random().Next(1, 10);
 Console.Write(" + " +numberB);
 int res = numberA+numberB;
 Console.WriteLine(" = " + res);

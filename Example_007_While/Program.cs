1a8506b4e593e2c105060fd6a94e3c774faf8da5
Lidiya <lesnikoval@yandex.ru>
2023-01-12 00:25:24 +0300

    Example 007 цикл

---
+++ Example_007_While/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int xa = 40, ya = 1,
+    xb = 1, yb = 25,
+    xc = 80, yс = 25;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yс);
+Console.WriteLine("+");
+
+int x = xa, y = ya;
+int count = 0;
+int what = 0;
+while (count<10)
+    what = new Random().Next(0, 3);
+    if (what==0)
+        x = (x+xa)/2;
+        y = (y+ya)/2;
+    if (what==1)
+        x = (x+xb)/2;
+        y = (y+yb)/2;
+    if (what==2)
+        x = (x+xc)/2;
+        y = (y+yс)/2;
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count+1;

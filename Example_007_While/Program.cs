b67b93183e461331839c1ee6a78bb77a42250f17
Lidiya <lesnikoval@yandex.ru>
2023-01-12 00:46:41 +0300

    Example 007 цикл правильный

--- Example_007_While/Program.cs
+++ Example_007_While/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Clear();
 int xa = 40, ya = 1,
-    xb = 1, yb = 25,
-    xc = 80, yс = 25;
+    xb = 1, yb = 20,
+    xc = 80, yс = 20;
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
 Console.SetCursorPosition(xb, yb);
@@ -12,17 +12,27 @@ Console.WriteLine("+");
 int x = xa, y = ya;
 int count = 0;
 int what = 0;
-while (count<10)
+while (count<10000)
+{
     what = new Random().Next(0, 3);
+    //Console.WriteLine(what);
     if (what==0)
+    {
         x = (x+xa)/2;
         y = (y+ya)/2;
+    }
     if (what==1)
+    {
         x = (x+xb)/2;
         y = (y+yb)/2;
+    }
     if (what==2)
+    {
         x = (x+xc)/2;
         y = (y+yс)/2;
+    }
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
     count = count+1;
+}
+//Console.WriteLine("Все");

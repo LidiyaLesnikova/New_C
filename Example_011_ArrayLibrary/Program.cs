c370b51a7af124adc2d7a8850425df16e81966f2
Lidiya <lesnikoval@yandex.ru>
2023-01-15 14:58:35 +0300

    Example 011 массивы функции и процедуры

---
+++ Example_011_ArrayLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArray(int[] collection)
+{
+    int leght = collection.Length;
+    int index = 0;
+    while (index<leght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.Write(coll[position]+"; ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int n = collection.Length;
+    int index = 0;
+    int pozition = -1;
+    while (index<n)
+    {
+        if (collection[index]== find) 
+        {
+            pozition = index;
+            break;
+        }
+        index++; 
+    }
+    return pozition;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine("Введите, позицию какого числа надо найти:");
+int find = Convert.ToInt32(Console.ReadLine());
+int pos = IndexOf(array, find);
+if (pos==-1) Console.WriteLine("Число в массиве не найдено");
+else Console.WriteLine("Первая позиция искомого числа - "+pos);

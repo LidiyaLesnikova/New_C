ad118b629f71aa1d4b84de5fef0fdedc4d1d0137
Lidiya <lesnikoval@yandex.ru>
2023-01-31 19:24:50 +0300

    Example 014 двумерный массив

---
+++ Example_014_2oy_Massiv/Program.cs
@@ -0,0 +1,28 @@
+﻿int[,] table = new int[3, 4];
+PrintArray(table);
+Console.WriteLine();
+FillArray(table);
+PrintArray(table);
+
+void PrintArray(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            Console.Write($"{matrix[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            matrix[rows, columns] = new Random().Next(1,10);
+        }
+    }
+}

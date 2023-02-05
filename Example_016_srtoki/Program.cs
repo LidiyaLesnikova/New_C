7308e57e2bacf79a3c96af7b02f282de2c6ad205
Lidiya <lesnikoval@yandex.ru>
2023-02-05 16:16:02 +0300

    Example 016 функции работы со строкой

---
+++ Example_016_srtoki/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+string text = "(1,2) (2,3) (4,5) (7,8)"
+                .Replace("(","")
+                .Replace(")","")
+                ;
+
+var data = text.Split(" ")  //разбить текст по пробелам
+               .Select(item => item.Split(',')) //выбрать элементы, разбитые ","
+               .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1]))) //разбить на кортежи
+               .Where(e => e.x%2==0)
+               .Select(point => (point.x*10, point.y+10 ))
+               .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+    // for (int k = 0; k < data[i].Length; k++)
+    // {
+    //     Console.WriteLine(data[i][k]);
+    // }
+    //Console.WriteLine(); 
+}
